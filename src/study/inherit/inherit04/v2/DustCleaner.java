89b577d530e51631550139dc7bdb4272e4e45d30
HEAMIN1011 <mintwo20@nate.com>
2024-12-23 16:59:56 +0900

    수정

--- src/study/inherit/inherit04/v2/DustCleaner.java
+++ src/study/inherit/inherit04/v2/DustCleaner.java
@@ -2,8 +2,8 @@ package study.inherit.inherit04.v2;
 
 public class DustCleaner extends Cleaner {
 	
-	public void removeDust( {
+	public void removeDust(){
 		System.out.println("먼지 털이");
-	})
+	}
 }
 
