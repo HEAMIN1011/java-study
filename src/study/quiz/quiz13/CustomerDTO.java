f89f7ef72540a1a6283eaeea6e376e33fad06aff
HEAMIN1011 <mintwo20@nate.com>
2025-01-07 12:12:28 +0900

    api dao dto 개념 및 퀴즈

---
+++ src/study/quiz/quiz13/CustomerDTO.java
@@ -0,0 +1,13 @@
+package study.quiz.quiz13;
+
+import java.util.List;
+
+public class CustomerDTO { //고객 정보 저장용 (포장박스)
+	
+	int id;
+	String name;
+	
+	List<Contact> contacts;
+	String address;
+	List<Order> orders;
+}
